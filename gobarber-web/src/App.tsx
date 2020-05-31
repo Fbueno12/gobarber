025c7d64b6d3aba9e1f5fdc8c33105c5340e6bd2
Felipe Bueno <felipebueno181@gmail.com>
2020-05-31 11:51:05 -0300

    [WEB] API de contexto

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -3,11 +3,15 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
-      {/* <SignUp /> */}
+      <AuthContext.Provider value={{ name: 'felipe' }}>
+        <SignIn />
+        {/* <SignUp /> */}
+      </AuthContext.Provider>
       <GlobalStyle />
     </>
   );
--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { FormHandles } from '@unform/core';
+import AuthContext from '../../context/AuthContext';
+
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
@@ -14,6 +16,10 @@ import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+
+  console.log(name);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
@@ -41,7 +47,7 @@ const SignIn: React.FC = () => {
         <img src={logoImg} alt="GoBarber" />
 
         <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu login</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
